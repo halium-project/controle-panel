a8d1e48999bbb57dc4d2bbe4106facb5ea1c4597
Peltoche <dev@halium.fr>
2019-02-16 19:26:55 +0100

    Commonize the header and footer for the logged pages

---
+++ src/components/pages/LoggedLayout.vue
@@ -0,0 +1,69 @@
+<template>
+  <Layout class="layout">
+  <Header>
+    <Menu mode="horizontal" theme="dark" active-name="1">
+      <div class="layout-logo"></div>
+      <div class="layout-nav">
+        <MenuItem name="1" to="/users">
+        <Icon type="md-contacts" />
+        Users
+        </MenuItem>
+        <MenuItem name="2" to="/applications">
+        <Icon type="md-appstore" />
+        Applications
+        </MenuItem>
+      </div>
+    </Menu>
+  </Header>
+  <Content class="content">
+    <router-view/>
+  </Content>
+  <Footer class="layout-footer-center">Halium Dashboard</Footer>
+  </Layout>
+</template>
+
+<script>
+export default {
+  name: 'LoggedLayout'
+}
+</script>
+
+<style scoped>
+.layout{
+  border: 1px solid #d7dde4;
+  background: #f5f7f9;
+  position: relative;
+  border-radius: 4px;
+  overflow: hidden;
+  min-height: 100%;
+}
+
+.layout-logo{
+  width: 100px;
+  height: 30px;
+  background: #5b6270;
+  border-radius: 3px;
+  float: left;
+  position: relative;
+  top: 15px;
+  left: 20px;
+}
+
+.layout-nav{
+  margin: 0 auto;
+  margin-left: 30px;
+  float: left;
+}
+
+.layout-footer-center{
+  text-align: center;
+}
+
+.content {
+  padding: '0 50px';
+  width: 90%;
+  margin-top: 30px;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,30 +2,42 @@ import Router from 'vue-router'
 
 import Callback from '@/utils/oauth2/Callback'
 
+import LoggedLayout from '@/components/pages/LoggedLayout'
 import Applications from '@/components/pages/Applications'
 import Login from '@/components/pages/Login'
 import Users from '@/components/pages/Users'
 
 let router = new Router({
   routes: [
-    {
-      path: '/',
-      redirect: '/users'
-    },
+    // Logged
     {
       path: '/applications',
       name: 'Applications',
-      component: Applications
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
+      component: LoggedLayout,
+      children: [{
+        path: '/',
+        component: Applications
+      }]
     },
     {
       path: '/users',
       name: 'Users',
-      component: Users
+      component: LoggedLayout,
+      children: [{
+        path: '/',
+        component: Users
+      }]
+    },
+    // Redirected
+    {
+      path: '/',
+      redirect: '/users'
+    },
+    // Not logged
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     },
     {
       path: '/access_token=*',
